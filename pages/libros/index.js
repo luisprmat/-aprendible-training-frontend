419218d96b7f6859070fb9625b4575f2b0b51d72
Luis Parrado <luisprmat@gmail.com>
2022-11-06 23:05:23 -0500

    Get books from API

--- pages/libros/index.js
+++ pages/libros/index.js
@@ -1,9 +1,25 @@
 import Link from "next/link";
 
-const BookList = () => {
+export async function getStaticProps() {
+  const res = await fetch('http://books-backend.test/api/books')
+  const data = await res.json()
+
+  return {
+    props: {
+      books: data
+    }
+  }
+}
+
+const BookList = ({ books }) => {
   return (
     <div>
       <h1>Libros</h1>
+      <ul>
+        {books.map(book => (
+          <li key={`book-${book.id}`}>{book.title}</li>
+        ))}
+      </ul>
 
       <Link href="/libros/crear">Create Book</Link>
     </div>
